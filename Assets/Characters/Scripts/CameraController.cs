141de6171a150dc6875827c2708be6f4b445cc34
Johan Andersson <johanderss@gmail.com>
2017-03-11 17:09:52 +0100

    Added camera control script

---
+++ Assets/Characters/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Camera mainCamera;
+    public float lookSpeed = 400f;
+
+    private Vector3 mainCameraInitialPosition;
+    private Quaternion mainCameraInitialRotation;
+
+    void Start()
+    {
+        mainCameraInitialRotation = mainCamera.transform.localRotation;
+        mainCameraInitialPosition = mainCamera.transform.position;
+    }
+
+    void Update()
+    {
+        bool freeLookPressed = Input.GetButton("Free Look");
+        bool freeLookUp = Input.GetButtonUp("Free Look");
+
+        if (freeLookPressed)
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            mainCamera.transform.RotateAround(transform.position, transform.up, -mouseX * Time.deltaTime * lookSpeed);
+            mainCamera.transform.RotateAround(transform.position, mainCamera.transform.right, mouseY * Time.deltaTime * lookSpeed);
+        }
+        else if (freeLookUp)
+        {
+            mainCamera.transform.position = mainCameraInitialPosition;
+            mainCamera.transform.localRotation = mainCameraInitialRotation;
+        }
+    }
+}
