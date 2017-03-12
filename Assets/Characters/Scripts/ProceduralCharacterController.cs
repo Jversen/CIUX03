e3b83fe836d22816810ee171ebb0f770c4eb256f
Johan Andersson <johanderss@gmail.com>
2017-03-12 16:54:21 +0100

    Working on animation using curves

---
+++ Assets/Characters/Scripts/ProceduralCharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralCharacterController : MonoBehaviour
+{
+
+    public Animator animator;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        bool jumpButtonDown = Input.GetButtonDown("Jump");
+
+        if (jumpButtonDown)
+        {
+            animator.SetTrigger("Jump");
+        }
+    }
+
+}
