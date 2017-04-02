6c8b09ea667418548c187492961f840f29f20c18
Johan Andersson <johanderss@gmail.com>
2017-04-02 13:25:32 +0200

    Now loading body parts from random character folder

--- Assets/Characters/Scripts/RandomCharacter.cs
+++ Assets/Characters/Scripts/RandomCharacter.cs
@@ -1,46 +1,47 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using UnityEngine;
 
 public class RandomCharacter : MonoBehaviour
 {
-    private Object[] chests;
-    private Object[] heads;
-    private Object[] legsLeft;
-    private Object[] legsRight;
-    private Object[] armsLeft;
-    private Object[] armsRight;
+    enum BodyParts { Chest, Head, LeftArm, RightArm, LeftLeg, RightLeg };
 
     private Object chestPrefab;
     private Object headPrefab;
-    private Object leftLegPrefab;
-    private Object rightLegPrefab;
     private Object leftArmPrefab;
     private Object rightArmPrefab;
+    private Object leftLegPrefab;
+    private Object rightLegPrefab;
 
     // GameObjects
+    private string charactersDir = "Prefabs/Characters/";
+    private string characterPath;
+    private string[] characterDirs;
     private GameObject chest;
     private GameObject head;
-    private GameObject leftLeg;
-    private GameObject rightLeg;
     private GameObject leftArm;
     private GameObject rightArm;
+    private GameObject leftLeg;
+    private GameObject rightLeg;
 
     void Start()
     {
-        chest = InstantiateBodyPart(RandomPrefab("Prefabs/Body Parts/Chests"), transform);
-        head = InstantiateBodyPart(RandomPrefab("Prefabs/Body Parts/Heads"), chest.transform);
-        leftLeg = InstantiateBodyPart(RandomPrefab("Prefabs/Body Parts/Legs Left"), chest.transform);
-        rightLeg = InstantiateBodyPart(RandomPrefab("Prefabs/Body Parts/Legs Right"), chest.transform);
-        leftArm = InstantiateBodyPart(RandomPrefab("Prefabs/Body Parts/Arms Left"), chest.transform);
-        rightArm = InstantiateBodyPart(RandomPrefab("Prefabs/Body Parts/Arms Right"), chest.transform);
+        characterPath = Application.dataPath + "/Resources/" + charactersDir;
+        characterDirs = Directory.GetDirectories(characterPath);
+
+        chest = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.Chest), transform);
+        head = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.Head), chest.transform);
+        leftArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftArm), chest.transform);
+        rightArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightArm), chest.transform);
+        leftLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftLeg), chest.transform);
+        rightLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightLeg), chest.transform);
 
         chest.transform.localPosition = Vector3.zero;
 
         attach(head, chest, chest.transform.Find("Head Socket"));
-        attach(leftLeg, chest, chest.transform.Find("Left Leg Socket"));
-        attach(rightLeg, chest, chest.transform.Find("Right Leg Socket"));
         attach(leftArm, chest, chest.transform.Find("Left Arm Socket"));
         attach(rightArm, chest, chest.transform.Find("Right Arm Socket"));
-
+        attach(leftLeg, chest, chest.transform.Find("Left Leg Socket"));
+        attach(rightLeg, chest, chest.transform.Find("Right Leg Socket"));
         // Freeze chest position for testing
         //chest.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
     }
@@ -50,10 +51,10 @@ public class RandomCharacter : MonoBehaviour
 
     }
 
-    private Object RandomPrefab(string folder)
+    private Object LoadRandomBodyPart(BodyParts bodyPart)
     {
-        Object[] objects = Resources.LoadAll(folder);
-        return objects[Random.Range(0, objects.Length)];
+        string character = Path.GetFileName(characterDirs[Random.Range(0, characterDirs.Length)]);
+        return Resources.Load(charactersDir + character + "/" + bodyPart);
     }
 
     private GameObject InstantiateBodyPart(Object bodyPart, Transform transform)
