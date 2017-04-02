f5da339af1747f440fcbc5876787c9461d14529f
Johan Andersson <johanderss@gmail.com>
2017-04-02 16:09:29 +0200

    Added lower legs to human character

--- Assets/Characters/Scripts/RandomCharacter.cs
+++ Assets/Characters/Scripts/RandomCharacter.cs
@@ -5,65 +5,44 @@ public class RandomCharacter : MonoBehaviour
 {
     enum BodyParts { Chest, Head, LeftUpperArm, RightUpperArm, LeftLowerArm, RightLowerArm, LeftHand, RightHand, LeftUpperLeg, RightUpperLeg, LeftLowerLeg, RightLowerLeg, LeftFoot, RightFoot };
 
-    private Object chestPrefab;
-    private Object headPrefab;
-    private Object leftArmPrefab;
-    private Object rightArmPrefab;
-    private Object leftLegPrefab;
-    private Object rightLegPrefab;
-
-    // GameObjects
+    // Body Parts
     private string charactersDir = "Prefabs/Characters/";
     private string characterPath;
     private string[] characterDirs;
+
     private GameObject chest;
     private GameObject head;
-    private GameObject leftArm;
-    private GameObject rightArm;
-    private GameObject leftLeg;
-    private GameObject rightLeg;
+    private GameObject leftUpperArm;
+    private GameObject rightUpperArm;
+    private GameObject leftUpperLeg;
+    private GameObject rightUpperLeg;
+    private GameObject leftLowerLeg;
+    private GameObject rightLowerLeg;
 
     void Start()
     {
         characterPath = Application.dataPath + "/Resources/" + charactersDir;
         characterDirs = Directory.GetDirectories(characterPath);
 
-        chest = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.Chest), transform);
-        head = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.Head), chest.transform);
-        leftArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftUpperArm), chest.transform);
-        rightArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightUpperArm), chest.transform);
-        leftLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftUpperLeg), chest.transform);
-        rightLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightUpperLeg), chest.transform);
+        chest = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.Chest), gameObject);
+        head = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.Head), chest);
+        leftUpperArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftUpperArm), chest);
+        rightUpperArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightUpperArm), chest);
+        leftUpperLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftUpperLeg), chest);
+        rightUpperLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightUpperLeg), chest);
+        leftLowerLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftLowerLeg), leftUpperLeg);
+        rightLowerLeg = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightLowerLeg), rightUpperLeg);
 
         chest.transform.localPosition = Vector3.zero;
 
-        if (chest != null)
-        {
-            if (head != null)
-            {
-                attach(head, chest, chest.transform.Find("Head Socket"));
-            }
+        attach(head, chest, chest.transform.Find("Head Socket"));
+        attach(leftUpperArm, chest, chest.transform.Find("Left Arm Socket"));
+        attach(rightUpperArm, chest, chest.transform.Find("Right Arm Socket"));
+        attach(leftUpperLeg, chest, chest.transform.Find("Left Leg Socket"));
+        attach(rightUpperLeg, chest, chest.transform.Find("Right Leg Socket"));
+        attach(leftLowerLeg, leftUpperLeg, leftUpperLeg.transform.Find("Lower Leg Socket"));
+        attach(rightLowerLeg, rightUpperLeg, rightUpperLeg.transform.Find("Lower Leg Socket"));
 
-            if (leftArm != null)
-            {
-                attach(leftArm, chest, chest.transform.Find("Left Arm Socket"));
-            }
-
-            if (rightArm != null)
-            {
-                attach(rightArm, chest, chest.transform.Find("Right Arm Socket"));
-            }
-
-            if (leftLeg != null)
-            {
-                attach(leftLeg, chest, chest.transform.Find("Left Leg Socket"));
-            }
-
-            if (rightLeg != null)
-            {
-                attach(rightLeg, chest, chest.transform.Find("Right Leg Socket"));
-            }
-        }
         // Freeze chest position for testing
         //chest.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
     }
@@ -79,14 +58,14 @@ public class RandomCharacter : MonoBehaviour
         return Resources.Load(charactersDir + character + "/" + bodyPart);
     }
 
-    private GameObject InstantiateBodyPart(Object bodyPart, Transform transform)
+    private GameObject InstantiateBodyPart(Object bodyPart, GameObject parent)
     {
         if (bodyPart == null)
         {
             return null;
         }
         GameObject bodyPartInst = Instantiate((GameObject)bodyPart);
-        bodyPartInst.transform.parent = transform;
+        bodyPartInst.transform.parent = parent.transform;
         return bodyPartInst;
     }
 
