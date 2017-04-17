145c9b3194b2a6b48a5bef0f221afb79605271fe
jversen <johan.iversen@gmail.com>
2017-04-17 20:34:48 +0200

    started ticking of equations

--- Assets/Scripts/MuscleBranchScripts/BalanceIntelligence.cs
+++ Assets/Scripts/MuscleBranchScripts/BalanceIntelligence.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkeletonMuscles : MonoBehaviour {
-	public Rigidbody leftLowerHamstring, leftUpperHamstring, rightLowerHamstring, rightUpperHamstring;
+public class BalanceIntelligence : MonoBehaviour {
+
+	/*public Rigidbody leftLowerHamstring, leftUpperHamstring, rightLowerHamstring, rightUpperHamstring;
 	public Rigidbody leftLowerGluteus, leftUpperGluteus, rightLowerGluteus, rightUpperGluteus;
 	public Rigidbody leftLowerHip, leftUpperHip, rightLowerHip, rightUpperHip;
 	public Rigidbody lowerAbs, upperAbs;
@@ -12,37 +13,25 @@ public class SkeletonMuscles : MonoBehaviour {
 	public Muscle leftGluteus, rightGluteus;
 	public Muscle leftHip, rightHip;
 	public Muscle abs;
-	public Muscle back;
-	public Rigidbody leftFoot, rightFoot ;
-	public float forceHamstring, forceGluteus, forceHip, forceAbs, forceBack;
+	public Muscle back; */
+	public Rigidbody leftFoot, rightFoot;
+	//public float forceHamstring, forceGluteus, forceHip, forceAbs, forceBack;
 	private bool leftFootContact = false; 
 	private bool rightFootContact = false;
 	private Vector3 leftFootCoG, rightFootCoG, bodyCoG, betweenFeetVector, leftToBodyCoG, rightToBodyCoG, leftFootGoalCoG, rightFootGoalCoG;
 	private float feetDistance, balanceLineDistance;
+
 	// Use this for initialization
 	void Start () {
-		leftHamstring = new Muscle (leftUpperHamstring, leftLowerHamstring);
-		rightHamstring = new Muscle (rightUpperHamstring, rightLowerHamstring);
-		leftGluteus = new Muscle (leftUpperGluteus, leftLowerGluteus);
-		rightGluteus = new Muscle (rightUpperGluteus, rightLowerGluteus);
-		leftHip = new Muscle (leftUpperHip, leftLowerHip);
-		rightHip = new Muscle (rightUpperHip, rightLowerHip);
-		abs = new Muscle (upperAbs, lowerAbs);
-		back = new Muscle (upperBack, lowerBack);
+		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
-		leftHamstring.MoveMuscle (forceHamstring);
-		rightHamstring.MoveMuscle (forceHamstring);
-		leftGluteus.MoveMuscle (forceGluteus);
-		rightGluteus.MoveMuscle (forceGluteus);
-		leftHip.MoveMuscle (forceHip);
-		rightHip.MoveMuscle (forceHip);
-		abs.MoveMuscle (forceAbs);
-		back.MoveMuscle (forceBack);
+		leftFootCoG = leftFoot.
+		rightFootCoG
 	}
+
 	void OnCollisionEnter(Collision coll){
 		print ("Entered OnCollisionEnter");
 		if (coll.contacts[0].thisCollider.gameObject.CompareTag("Ground")){
@@ -78,22 +67,5 @@ public class SkeletonMuscles : MonoBehaviour {
 		}
 		print ("right foot: " + rightFootContact);
 		print ("left foot: " + leftFootContact);
-	}
-	public class Muscle{
-		private Rigidbody upper, lower;
-		public Muscle(Rigidbody upper, Rigidbody lower)
-		{
-			this.upper = upper;
-			this.lower = lower;
-		}
-		public void MoveMuscle(float force)
-		{
-			Vector3 direction1 = upper.transform.position - lower.transform.position;
-			direction1.Normalize();
-			direction1 = direction1 * force;
-			Vector3 direction2 = -direction1;
-			upper.AddForce(direction2 * Time.deltaTime);
-			lower.AddForce(direction1 * Time.deltaTime);
-		}
 	}
 }
