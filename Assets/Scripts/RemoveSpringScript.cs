5693e32319cdf88df8bdd010de383f1c3f7c197b
leigh <lagebergman@gmail.com>
2017-04-18 12:40:02 +0200

    Add script for moving objects with springs

---
+++ Assets/Scripts/RemoveSpringScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveSpringScript : MonoBehaviour {
+
+	public GameObject connector;
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.Equals(connector)){
+			gameObject.GetComponent<Rigidbody> ().isKinematic = false;
+			Destroy(connector.GetComponent<SpringJoint> ());
+			Destroy(this);
+		}
+	}
+}
---
+++ Assets/Scripts/SpringScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringScript : MonoBehaviour {
+
+	public Rigidbody connector;
+
+	// Update is called once per frame
+	void OnMouseDown(){
+		if (connector != null) {
+			gameObject.AddComponent<SpringJoint> ();
+			SpringJoint joint = gameObject.GetComponent<SpringJoint> ();
+			Vector3 connectorPosition = connector.transform.position;
+			connector.isKinematic = true;
+			connector.gameObject.AddComponent<RemoveSpringScript> ();
+			connector.gameObject.GetComponent<RemoveSpringScript> ().connector = gameObject;
+			connector.transform.position = gameObject.transform.position;
+			joint.enableCollision = true;
+			joint.connectedBody = connector;
+			connector.transform.position = connectorPosition;
+		}
+	}
+}
