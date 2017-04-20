b232616175e9f001b9e517b06a1e8281aeb16170
Erik Hildinge <Erik Hildinge>
2017-04-20 17:38:03 +0200

    Add a new way to pick up a sword

--- Assets/Scripts/GrabScript.cs
+++ Assets/Scripts/GrabScript.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class GrabScript : MonoBehaviour {
 
+	public GameObject sword;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -16,6 +17,10 @@ public class GrabScript : MonoBehaviour {
 
 	void OnCollisionEnter(Collision col){
 		if(col.gameObject.tag.Equals("Hand")){
+			// Flyttar svärdet så att handtaget är i handen. Kommer behöva justeras beroende på karaktär om detta format behålls.
+			// Går ej att flytta handle till positionen, då svärdet går sönder.
+			sword.transform.position = new Vector3(col.gameObject.transform.position[0], col.gameObject.transform.position[1] + (0.85f), col.gameObject.transform.position[2]);
+
 			/*Component handle = GetComponent ("Handle");
 			if (handle != null) {
 				col.gameObject.transform.position = handle.transform.position;
