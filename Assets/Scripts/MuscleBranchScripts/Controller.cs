58ada07b690e677ca51c4b3fd38fe8bb989fb652
leigh <lagebergman@gmail.com>
2017-05-05 12:41:28 +0200

    Make Controller use relative directions

--- Assets/Scripts/MuscleBranchScripts/Controller.cs
+++ Assets/Scripts/MuscleBranchScripts/Controller.cs
@@ -13,20 +13,21 @@ public class Controller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//GetComponent<Renderer> ().bounds.size;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (left)) {
-			head.AddForce (0, 0,force);
+			head.AddForce (transform.forward * force);
+			//head.AddTorque(0,-force,0);
 		} else if (Input.GetKey (right)) {
-				head.AddForce (0, 0, -force);
+			head.AddForce (transform.forward * -force);
+			//head.AddTorque(0, force,0);
 		} else if (Input.GetKey (up)) {
-			head.AddForce (force, 0, 0);
+			head.AddForce (transform.right * force); //TODO: Change this if forward is no longer left.
 		} else if (Input.GetKey (down)) {
-				head.AddForce (-force, 0, 0);
+			head.AddForce (transform.right * -force);
 		} 
-			
-
 	}
 }
