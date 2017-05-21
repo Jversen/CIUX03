c7f7f4f1f3d58e499c48509010043e52c02d1a85
Johan Andersson <johanderss@gmail.com>
2017-05-21 12:59:32 +0200

    Worked with random character demo scene.

--- Assets/Characters/Scripts/RandomCharacter.cs
+++ Assets/Characters/Scripts/RandomCharacter.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 public class RandomCharacter : MonoBehaviour
 {
     enum BodyParts { Chest, Head, LeftUpperArm, RightUpperArm, LeftLowerArm, RightLowerArm, LeftHand, RightHand, LeftUpperLeg, RightUpperLeg, LeftLowerLeg, RightLowerLeg, LeftFoot, RightFoot, Tail };
-	public float bodyPartMass;
-	private bool isQuadruped;
-	// Body Parts
+    public float bodyPartMass;
+    private bool isQuadruped;
+    // Body Parts
     private string charactersDir = "Prefabs/Characters/";
     private string characterPath;
     private string[] characterDirs;
 
-	public string horizontalAxis;
-	public string verticalAxis;
+    public bool human = false;
+    public bool horse = false;
+    public bool tRex = false;
+
+    public string horizontalAxis;
+    public string verticalAxis;
 
     private GameObject chest;
     private GameObject head;
@@ -28,52 +32,56 @@ public class RandomCharacter : MonoBehaviour
     private GameObject rightLowerLeg;
     private GameObject leftFoot;
     private GameObject rightFoot;
-	private GameObject tail;
+    private GameObject tail;
 
-	private GameObject leftFistHome;
-	private GameObject leftFistAway;
+    private GameObject leftFistHome;
+    private GameObject leftFistAway;
 
     void Start()
     {
-		//Mass should not be lower than .1;
-		if (bodyPartMass < 0.1f) {
-			bodyPartMass = 0.1f;
-		}
+        //Mass should not be lower than .1;
+        if (bodyPartMass < 0.1f)
+        {
+            bodyPartMass = 0.1f;
+        }
         characterPath = Application.dataPath + "/Resources/" + charactersDir;
         characterDirs = Directory.GetDirectories(characterPath);
 
         // Instantiate objects
-		string chestPath = LoadRandomBodyPath ();
-		isQuadruped = chestPath.Equals ("Horse");
-		chest = InstantiateBodyPart(Resources.Load(charactersDir + chestPath + "/" + BodyParts.Chest), gameObject);
+        string chestPath = LoadRandomBodyPath();
+        isQuadruped = chestPath.Equals("Horse");
+        chest = InstantiateBodyPart(Resources.Load(charactersDir + chestPath + "/" + BodyParts.Chest), gameObject);
         head = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.Head), chest);
-        leftUpperArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftUpperArm), chest);
-        rightUpperArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightUpperArm), chest);
-        leftLowerArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftLowerArm), leftUpperArm);
-        rightLowerArm = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightLowerArm), rightUpperArm);
-        leftHand = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.LeftHand), leftLowerArm);
-        rightHand = InstantiateBodyPart(LoadRandomBodyPart(BodyParts.RightHand), rightLowerArm);
-		string upperLegPath = LoadRandomBodyPath ();
-		leftUpperLeg = InstantiateBodyPart(Resources.Load(charactersDir + upperLegPath + "/" + BodyParts.LeftUpperLeg), chest);
-		rightUpperLeg = InstantiateBodyPart(Resources.Load(charactersDir + upperLegPath + "/" + BodyParts.RightUpperLeg), chest);
-		string lowerLegPath = LoadRandomBodyPath ();
-		leftLowerLeg = InstantiateBodyPart(Resources.Load(charactersDir + lowerLegPath + "/" + BodyParts.LeftLowerLeg), leftUpperLeg);
-		rightLowerLeg = InstantiateBodyPart(Resources.Load(charactersDir + lowerLegPath + "/" + BodyParts.RightLowerLeg), rightUpperLeg);
-		string footPath = LoadRandomBodyPath ();
-		leftFoot = InstantiateBodyPart(Resources.Load(charactersDir + footPath + "/" + BodyParts.LeftFoot), leftLowerLeg);
-		rightFoot = InstantiateBodyPart(Resources.Load(charactersDir + footPath + "/" + BodyParts.RightFoot), rightLowerLeg);
+        string upperArmPath = LoadRandomBodyPath();
+        leftUpperArm = InstantiateBodyPart(Resources.Load(charactersDir + upperArmPath + "/" + BodyParts.LeftUpperArm), chest);
+        rightUpperArm = InstantiateBodyPart(Resources.Load(charactersDir + upperArmPath + "/" + BodyParts.RightUpperArm), chest);
+        string lowerArmPath = LoadRandomBodyPath();
+        leftLowerArm = InstantiateBodyPart(Resources.Load(charactersDir + lowerArmPath + "/" + BodyParts.LeftLowerArm), leftUpperArm);
+        rightLowerArm = InstantiateBodyPart(Resources.Load(charactersDir + lowerArmPath + "/" + BodyParts.RightLowerArm), rightUpperArm);
+        string handPath = LoadRandomBodyPath();
+        leftHand = InstantiateBodyPart(Resources.Load(charactersDir + handPath + "/" + BodyParts.LeftHand), leftLowerArm);
+        rightHand = InstantiateBodyPart(Resources.Load(charactersDir + handPath + "/" + BodyParts.RightHand), rightLowerArm);
+        string upperLegPath = LoadRandomBodyPath();
+        leftUpperLeg = InstantiateBodyPart(Resources.Load(charactersDir + upperLegPath + "/" + BodyParts.LeftUpperLeg), chest);
+        rightUpperLeg = InstantiateBodyPart(Resources.Load(charactersDir + upperLegPath + "/" + BodyParts.RightUpperLeg), chest);
+        string lowerLegPath = LoadRandomBodyPath();
+        leftLowerLeg = InstantiateBodyPart(Resources.Load(charactersDir + lowerLegPath + "/" + BodyParts.LeftLowerLeg), leftUpperLeg);
+        rightLowerLeg = InstantiateBodyPart(Resources.Load(charactersDir + lowerLegPath + "/" + BodyParts.RightLowerLeg), rightUpperLeg);
+        string footPath = LoadRandomBodyPath();
+        leftFoot = InstantiateBodyPart(Resources.Load(charactersDir + footPath + "/" + BodyParts.LeftFoot), leftLowerLeg);
+        rightFoot = InstantiateBodyPart(Resources.Load(charactersDir + footPath + "/" + BodyParts.RightFoot), rightLowerLeg);
 
-		//Set the mass of the upper body to bodyPartMass
-		ChangeMassOfUpperBody();
-			
-		UpperBodyPID upperBodyPID = chest.AddComponent<UpperBodyPID> ();
-		upperBodyPID.isQuadruped = isQuadruped;
+        //Set the mass of the upper body to bodyPartMass
+        ChangeMassOfUpperBody();
 
-		Object tailObj = LoadRandomBodyPart(BodyParts.Tail);
+        UpperBodyPID upperBodyPID = chest.AddComponent<UpperBodyPID>();
+        upperBodyPID.isQuadruped = isQuadruped;
+
+        Object tailObj = LoadRandomBodyPart(BodyParts.Tail);
         if (tailObj != null)
         {
             tail = InstantiateBodyPart(tailObj, chest);
-			tail.GetComponent<Rigidbody> ().mass = bodyPartMass;
+            tail.GetComponent<Rigidbody>().mass = bodyPartMass;
         }
 
         chest.transform.localPosition = Vector3.zero;
@@ -83,104 +91,108 @@ public class RandomCharacter : MonoBehaviour
         attach(leftUpperArm, chest, chest.transform.Find("Left Arm Socket"));
         attach(rightUpperArm, chest, chest.transform.Find("Right Arm Socket"));
         attachSliceable(leftLowerArm, leftUpperArm, leftUpperArm.transform.Find("Lower Arm Socket"));
-		attachSliceable(rightLowerArm, rightUpperArm, rightUpperArm.transform.Find("Lower Arm Socket"));
-		attachSliceable(leftHand, leftLowerArm, leftLowerArm.transform.Find("Hand Socket"));
-		attachSliceable(rightHand, rightLowerArm, rightLowerArm.transform.Find("Hand Socket"));
-		attach(leftUpperLeg, chest, chest.transform.Find("Left Leg Socket"));
-		attach(rightUpperLeg, chest, chest.transform.Find("Right Leg Socket"));
-		attachSliceable(leftLowerLeg, leftUpperLeg, leftUpperLeg.transform.Find("Lower Leg Socket"));
-		attachSliceable(rightLowerLeg, rightUpperLeg, rightUpperLeg.transform.Find("Lower Leg Socket"));
-		attachSliceable(leftFoot, leftLowerLeg, leftLowerLeg.transform.Find("Foot Socket"));
-		attachSliceable(rightFoot, rightLowerLeg, rightLowerLeg.transform.Find("Foot Socket"));
+        attachSliceable(rightLowerArm, rightUpperArm, rightUpperArm.transform.Find("Lower Arm Socket"));
+        attachSliceable(leftHand, leftLowerArm, leftLowerArm.transform.Find("Hand Socket"));
+        attachSliceable(rightHand, rightLowerArm, rightLowerArm.transform.Find("Hand Socket"));
+        attach(leftUpperLeg, chest, chest.transform.Find("Left Leg Socket"));
+        attach(rightUpperLeg, chest, chest.transform.Find("Right Leg Socket"));
+        attachSliceable(leftLowerLeg, leftUpperLeg, leftUpperLeg.transform.Find("Lower Leg Socket"));
+        attachSliceable(rightLowerLeg, rightUpperLeg, rightUpperLeg.transform.Find("Lower Leg Socket"));
+        attachSliceable(leftFoot, leftLowerLeg, leftLowerLeg.transform.Find("Foot Socket"));
+        attachSliceable(rightFoot, rightLowerLeg, rightLowerLeg.transform.Find("Foot Socket"));
 
         if (tail != null)
         {
             attach(tail, chest, chest.transform.Find("Tail Socket"));
         }
-		/*
+        /*
 		EnablePunching ();
 		EnableUserInput ();
 		EnableFootPlacement ();
 		*/
 
-		//Make feet rotation horizontal
-		float rightFootYRot = rightFoot.transform.rotation.eulerAngles.y;
-		rightFoot.transform.rotation = Quaternion.Euler(0, rightFootYRot, 0);
-		float leftFootYRot = leftFoot.transform.rotation.eulerAngles.y;
-		leftFoot.transform.rotation = Quaternion.Euler(0, leftFootYRot, 0);
-		//Move character down to make feet touch the ground.
-		Vector3 footHeight = new Vector3 (0, rightFoot.transform.position.y, 0);
-		chest.transform.parent.position -= footHeight;
+        //Make feet rotation horizontal
+        float rightFootYRot = rightFoot.transform.rotation.eulerAngles.y;
+        rightFoot.transform.rotation = Quaternion.Euler(0, rightFootYRot, 0);
+        float leftFootYRot = leftFoot.transform.rotation.eulerAngles.y;
+        leftFoot.transform.rotation = Quaternion.Euler(0, leftFootYRot, 0);
+        //Move character down to make feet touch the ground.
+        Vector3 footHeight = new Vector3(0, rightFoot.transform.position.y, 0);
+        chest.transform.parent.position -= footHeight;
 
         // Freeze chest position for testing
-		//chest.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        //chest.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
     }
 
-	private void ChangeMassOfUpperBody(){
+    private void ChangeMassOfUpperBody()
+    {
 
-		chest.GetComponent<Rigidbody> ().mass = bodyPartMass;
-		head.GetComponent<Rigidbody> ().mass = bodyPartMass;
+        chest.GetComponent<Rigidbody>().mass = bodyPartMass;
+        head.GetComponent<Rigidbody>().mass = bodyPartMass;
 
-		//If it is not a quadruped it's arms are part of the upper body and thus their masses should be changed;
-		if (!isQuadruped) {
-			leftUpperArm.GetComponent<Rigidbody> ().mass = bodyPartMass;
-			rightUpperArm.GetComponent<Rigidbody> ().mass = bodyPartMass;
-			leftLowerArm.GetComponent<Rigidbody> ().mass = bodyPartMass;
-			rightLowerArm.GetComponent<Rigidbody> ().mass = bodyPartMass;
-			leftHand.GetComponent<Rigidbody> ().mass = bodyPartMass;
-			rightHand.GetComponent<Rigidbody> ().mass = bodyPartMass;
-		}
+        //If it is not a quadruped it's arms are part of the upper body and thus their masses should be changed;
+        if (!isQuadruped)
+        {
+            leftUpperArm.GetComponent<Rigidbody>().mass = bodyPartMass;
+            rightUpperArm.GetComponent<Rigidbody>().mass = bodyPartMass;
+            leftLowerArm.GetComponent<Rigidbody>().mass = bodyPartMass;
+            rightLowerArm.GetComponent<Rigidbody>().mass = bodyPartMass;
+            leftHand.GetComponent<Rigidbody>().mass = bodyPartMass;
+            rightHand.GetComponent<Rigidbody>().mass = bodyPartMass;
+        }
 
-		if (tail != null)
-		{
-			tail.GetComponent<Rigidbody> ().mass = bodyPartMass;
-		}
-		
-	}
+        if (tail != null)
+        {
+            tail.GetComponent<Rigidbody>().mass = bodyPartMass;
+        }
 
-	//Create two regions near left arm of character for using as spring anchors when punching
-	private void EnablePunching(){
+    }
+
+    //Create two regions near left arm of character for using as spring anchors when punching
+    private void EnablePunching()
+    {
 
 
-		leftFistHome = InstantiateBodyPart (Resources.Load(charactersDir + "TriggerRegion"),chest);
-		leftFistAway = InstantiateBodyPart (Resources.Load(charactersDir + "TriggerRegion"),chest);
-		PunchingScript punchingScript = leftFistAway.AddComponent<PunchingScript> ();
+        leftFistHome = InstantiateBodyPart(Resources.Load(charactersDir + "TriggerRegion"), chest);
+        leftFistAway = InstantiateBodyPart(Resources.Load(charactersDir + "TriggerRegion"), chest);
+        PunchingScript punchingScript = leftFistAway.AddComponent<PunchingScript>();
 
-		punchingScript.fist = leftHand.GetComponent <Rigidbody>();
-		punchingScript.home = leftFistHome.GetComponent<Rigidbody>();
+        punchingScript.fist = leftHand.GetComponent<Rigidbody>();
+        punchingScript.home = leftFistHome.GetComponent<Rigidbody>();
 
-		float upperArmLength = GetLongest (leftUpperArm.GetComponent<Renderer> ().bounds.size);
-		float armLength = GetLongest (leftLowerArm.GetComponent<Renderer> ().bounds.size) + upperArmLength;
+        float upperArmLength = GetLongest(leftUpperArm.GetComponent<Renderer>().bounds.size);
+        float armLength = GetLongest(leftLowerArm.GetComponent<Renderer>().bounds.size) + upperArmLength;
 
-		leftHand.GetComponent<Rigidbody> ().mass = 0.1f;
-		leftUpperArm.GetComponent<Rigidbody> ().mass = 0.1f;
-		leftLowerArm.GetComponent<Rigidbody> ().mass = 0.1f;
+        leftHand.GetComponent<Rigidbody>().mass = 0.1f;
+        leftUpperArm.GetComponent<Rigidbody>().mass = 0.1f;
+        leftLowerArm.GetComponent<Rigidbody>().mass = 0.1f;
 
-		Vector3 leftShoulderPos = chest.transform.Find ("Left Arm Socket").position;
-		leftFistHome.transform.position = leftShoulderPos + leftFistHome.transform.forward * upperArmLength - leftFistHome.transform.up * 0.1f;
-		leftFistAway.transform.position = leftShoulderPos + leftFistAway.transform.forward * armLength - leftFistAway.transform.up * 0.1f;
+        Vector3 leftShoulderPos = chest.transform.Find("Left Arm Socket").position;
+        leftFistHome.transform.position = leftShoulderPos + leftFistHome.transform.forward * upperArmLength - leftFistHome.transform.up * 0.1f;
+        leftFistAway.transform.position = leftShoulderPos + leftFistAway.transform.forward * armLength - leftFistAway.transform.up * 0.1f;
 
-		Vector3 leftHandPos = leftHand.transform.position;
-		leftHand.transform.position = leftFistHome.transform.position;
-		SpringJoint leftHandSpring = leftHand.AddComponent<SpringJoint> ();
-		leftHandSpring.connectedBody = leftFistHome.GetComponent<Rigidbody> ();
-		leftHandSpring.enableCollision = true;
-		leftHandSpring.spring = 8f;
-		leftHand.transform.position = leftHandPos;
-		//TODO: Fine tune punch region positions
+        Vector3 leftHandPos = leftHand.transform.position;
+        leftHand.transform.position = leftFistHome.transform.position;
+        SpringJoint leftHandSpring = leftHand.AddComponent<SpringJoint>();
+        leftHandSpring.connectedBody = leftFistHome.GetComponent<Rigidbody>();
+        leftHandSpring.enableCollision = true;
+        leftHandSpring.spring = 8f;
+        leftHand.transform.position = leftHandPos;
+        //TODO: Fine tune punch region positions
 
-	}
+    }
 
-	private void EnableUserInput(){
-		InputControl controller = gameObject.AddComponent<InputControl> ();
-		controller.characterChest = chest.GetComponent<Rigidbody> ();
-		controller.forceConstant = 1000;
-		controller.rotationSpeed = 1000;
-		controller.horizontalAxis = horizontalAxis;
-		controller.verticalAxis = verticalAxis;
-	}
+    private void EnableUserInput()
+    {
+        InputControl controller = gameObject.AddComponent<InputControl>();
+        controller.characterChest = chest.GetComponent<Rigidbody>();
+        controller.forceConstant = 1000;
+        controller.rotationSpeed = 1000;
+        controller.horizontalAxis = horizontalAxis;
+        controller.verticalAxis = verticalAxis;
+    }
 
-	/*private void EnableFootPlacement(){
+    /*private void EnableFootPlacement(){
 		PIDControl leftLowerLegPidControl = gameObject.AddComponent<PIDControl> ();
 		leftLowerLegPidControl.body = leftLowerLeg.GetComponent<Rigidbody> ();
 		leftLowerLegPidControl.mfConstant = 1500;
@@ -207,22 +219,37 @@ public class RandomCharacter : MonoBehaviour
 		balanceIntelligence.bodyCore = chest.GetComponent<Rigidbody> ();
 	}*/
 
-	//Sätt en fästpunkt för den högra underarmen. Spring joint.
+    //Sätt en fästpunkt för den högra underarmen. Spring joint.
 
     void Update()
     {
 
     }
 
-	private string LoadRandomBodyPath()
-	{
-		return Path.GetFileName(characterDirs[Random.Range (0, characterDirs.Length)]);
-	}
+    private string LoadRandomBodyPath()
+    {
+        if (human)
+        {
+            return "Human";
+        }
+        else if (horse)
+        {
+            return "Horse";
+        }
+        else if (tRex)
+        {
+            return "T-Rex";
+        }
+        else
+        {
+            return Path.GetFileName(characterDirs[Random.Range(0, characterDirs.Length)]);
+        }
+    }
 
     private Object LoadRandomBodyPart(BodyParts bodyPart)
-    {	
-		string character = LoadRandomBodyPath ();
-		return Resources.Load(charactersDir + character + "/" + bodyPart);
+    {
+        string character = LoadRandomBodyPath();
+        return Resources.Load(charactersDir + character + "/" + bodyPart);
     }
 
     private GameObject InstantiateBodyPart(Object bodyPart, GameObject parent)
@@ -232,8 +259,8 @@ public class RandomCharacter : MonoBehaviour
             return null;
         }
         GameObject bodyPartInst = Instantiate((GameObject)bodyPart);
-		bodyPartInst.transform.parent = parent.transform;
-		bodyPartInst.transform.rotation = parent.transform.rotation;
+        bodyPartInst.transform.parent = parent.transform;
+        bodyPartInst.transform.rotation = parent.transform.rotation;
         return bodyPartInst;
     }
 
@@ -245,28 +272,32 @@ public class RandomCharacter : MonoBehaviour
         joint.connectedAnchor = attachToTransform.localPosition;
 
         objectToAttach.transform.localPosition = attachToTransform.localPosition - joint.anchor / 2;
-	}
+    }
 
-	private void attachSliceable(GameObject objectToAttach, GameObject attachToSliceable, Transform attachToTransform)
-	{
-		attach (objectToAttach, attachToSliceable, attachToTransform);
-		attachToSliceable.AddComponent<Sliceable> ();
-		attachToSliceable.AddComponent<JointDestroyerSliceable> ();
-		attachToSliceable.GetComponent<JointDestroyerSliceable> ().connector = objectToAttach;
-	}
+    private void attachSliceable(GameObject objectToAttach, GameObject attachToSliceable, Transform attachToTransform)
+    {
+        attach(objectToAttach, attachToSliceable, attachToTransform);
+        attachToSliceable.AddComponent<Sliceable>();
+        attachToSliceable.AddComponent<JointDestroyerSliceable>();
+        attachToSliceable.GetComponent<JointDestroyerSliceable>().connector = objectToAttach;
+    }
 
-	public float GetLongest(Vector3 vector){
-		float longest = vector.x;
-		if (vector.y > longest) {
-			longest = vector.y;
-		}
-		if (vector.z > longest) {
-			longest = vector.z;
-		}
-		return longest;
-	}
+    public float GetLongest(Vector3 vector)
+    {
+        float longest = vector.x;
+        if (vector.y > longest)
+        {
+            longest = vector.y;
+        }
+        if (vector.z > longest)
+        {
+            longest = vector.z;
+        }
+        return longest;
+    }
 
-	public bool IsQuadruped(){
-		return isQuadruped;
-	}
+    public bool IsQuadruped()
+    {
+        return isQuadruped;
+    }
 }
